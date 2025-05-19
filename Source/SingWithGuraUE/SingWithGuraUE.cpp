75cfe0752c16f62940bf8ea549df7a816f93de7a
itsGawr-Gura <itsgawrgura@gmail.com>
2025-05-19 19:25:54 +0300

    Добавьте файлы проекта.

---
+++ Source/SingWithGuraUE/SingWithGuraUE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SingWithGuraUE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SingWithGuraUE, "SingWithGuraUE" );
